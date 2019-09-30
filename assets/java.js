87e92d73daa29ed2cf717f80dff8e9d1c2fefc94
setheugene <seth.eugene@icloud.com>
2019-09-29 21:21:29 -0500

    sullyswitch

--- assets/java.js
+++ assets/java.js
@@ -11,27 +11,27 @@ $(document).ready(function() {
     
     // }
 
-    var toAdd = document.createDocumentFragment();
-for(var i=1; i < 101; i++){
-   var newDiv = document.createElement('div');
-   newDiv.id = 'b_'+i;
-   newDiv.className = 'block';
-   toAdd.appendChild(newDiv);
-   if (i %2){
-       newDiv.className='block two'
-   }else{
-       newDiv.className='block one'
-   }
-}
+//     var toAdd = document.createDocumentFragment();
+// for(var i=1; i < 101; i++){
+//    var newDiv = document.createElement('div');
+//    newDiv.id = 'b_'+i;
+//    newDiv.className = 'block';
+//    toAdd.appendChild(newDiv);
+//    if (i %2){
+//        newDiv.className='block two'
+//    }else{
+//        newDiv.className='block one'
+//    }
+// }
 
-document.getElementById('shapes').appendChild(toAdd);
+// document.getElementById('shapes').appendChild(toAdd);
 
-    // $("#container").addClass('block2');
-    $(".one").click(function(){
-        $(this).addClass('move-left');
-    });
+//     // $("#container").addClass('block2');
+//     $(".one").click(function(){
+//         $(this).addClass('move-left');
+//     });
 
-    $(".two").click(function(){
-        $(this).addClass('move-right');
-    });
+//     $(".two").click(function(){
+//         $(this).addClass('move-right');
+//     });
 });
